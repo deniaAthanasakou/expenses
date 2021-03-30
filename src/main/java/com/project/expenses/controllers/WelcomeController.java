933cce35bc54031975ea978b5c69687d84acd8bc
deniaAthanasakou <antonia.ath@gmail.com>
2021-03-31 01:22:18 +0300

    Added css and property injection

--- src/main/java/com/project/expenses/controllers/WelcomeController.java
+++ src/main/java/com/project/expenses/controllers/WelcomeController.java
@@ -1,23 +1,26 @@
 package com.project.expenses.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Map;
 import java.util.logging.Logger;
 
 @Controller
 public class WelcomeController {
 
     private final static Logger LOGGER = Logger.getLogger(WelcomeController.class.getName());
-    // inject via application.properties
-    //@Value("${welcome.message:test}")
-    private String message = "Hello World";
 
-    @RequestMapping("/welcome")
-    public ModelAndView firstPage() {
-        LOGGER.info("Accessed Welcome controller");
-        return new ModelAndView("welcome");
+    // inject via application.properties
+    @Value("${welcome.message}")
+    private String message;
+
+    @RequestMapping("/")
+    public String welcome(Map<String, Object> model) {
+        model.put("message", this.message);
+        return "welcome";
     }
 
 }
